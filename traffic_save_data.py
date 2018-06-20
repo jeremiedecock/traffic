a6bc1ce10669d44b0d137357bbec1002a7b048dd
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-06-20 11:19:46 +0200

    Fix several bugs.

--- traffic_save_data.py
+++ traffic_save_data.py
@@ -3,24 +3,28 @@
 
 import numpy as np
 import json
+import time
 
 from datetime import datetime
 from urllib.request import urlopen
 
+# Get the Google Maps API private key
+api_key = np.genfromtxt('api.key', dtype='unicode')
+
 # TODO: put the 4 next variables in an external configuration file
-origin = 'Orsay'
-destination = 'Paris'
+#origin = 'Greenwich,England'
+#destination = 'Stockholm,Sweden'
+origin = '48.8598263,2.3156588'     # GPS coordinates
+destination = '48.853157,2.369259'  # GPS coordinates
 request_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=%s&destinations=%s&departure_time=now&traffic_model=best_guess&key=%s' % (origin, destination, api_key)
 wait_time_sec = 180  # in secs
 
 def get_duration(request_url):
     content = urlopen(request_url).read().decode('utf8')
     data = json.loads(content)
+    #print(data)
     duration = data['rows'][0]['elements'][0]['duration_in_traffic']['value']
     return duration / 60.
-
-# Get the Google Maps API private key
-api_key = np.genfromtxt('api.key', dtype='unicode')
 
 # TODO: improve the next 3 lines: set a duration instead a end time and make it configurable to be able to launch the script several times per day with different durations...
 now = datetime.now()
@@ -31,7 +35,7 @@ times = []
 durations = []
 
 # TODO: improve the next 3 lines (create the data file)...
-data_file = 'traffic-archive/traffic_' + now.strftime('%Y-%m-%d') + '.csv'
+data_file = 'traffic_archive/traffic_' + now.strftime('%Y-%m-%d') + '.csv'
 fd = open(data_file, 'w')
 fd.close()
 
@@ -53,5 +57,7 @@ while now < end:
 
     durations.append(duration)
     times.append(date)
+
+    time.sleep(wait_time_sec)
 
     now = datetime.now()
